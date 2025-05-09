c8f4abe4d8bff0fc5b1cc2a587beb1d91723bc29
JingyangQiao <159891886+JingyangQiao@users.noreply.github.com>
2025-05-10 03:36:50 +0800

    Update 1_Science.sh

--- scripts/CoIN/Train_MOE/1_Science.sh
+++ scripts/CoIN/Train_MOE/1_Science.sh
@@ -14,7 +14,7 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port 29600 llava/train/tr
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --expert_num 2 \
     --model_name_or_path checkpoints/Vicuna/vicuna-7b-v1.5 \
-    --pretrain_mm_mlp_adapter ./checkpoints/Instruction/Only_Pretrain_1.5/llava-vicuna-2-7b-chat-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/Instruction/Only_Pretrain_1.5_7b/llava-vicuna-2-7b-chat-pretrain/mm_projector.bin \
     --version $PROMPT_VERSION \
     --data_path ./playground/Instructions/ScienceQA/train.json \
     --image_folder ./cl_dataset \
